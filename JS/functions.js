ada3366ff8eb9ee82cfdcdb943e5b4e0cc822824
Mo'men EzzatMo'men EzzatMo'men Ezzat <smarty21000@gmail.com>
2016-10-11 23:01:11 +0300

    Added rest of the pages

--- JS/functions.js
+++ JS/functions.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+    var loginForm = $("#login"),
+        loginBtn = $("#login input[type='submit']");
 
+
+    // Admin Login Form validition
+    loginForm.on("submit", function(event) {
+        event.preventDefault();
+        var loginUser = $("#login input[name='username']").val(),
+            loginPassword = $("#login input[type='password']").val(),
+            adminCred = "admin";
+        console.log(loginPassword, loginUser);
+        if (loginUser == adminCred && loginPassword == adminCred) {
+            window.location.href = '../admin.html';
+        } else {
+            window.location.href = '../';
+        }
+    })
+
+});
